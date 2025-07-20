568d134cc972a361a90e2dd7898fae2b6bb969be
Suraj Shetty <suraj0589@gmail.com>
2025-07-20 22:22:31 +0530

    Added custom weather background to the main screen

--- lib/views/weather_screen.dart
+++ lib/views/weather_screen.dart
@@ -3,7 +3,9 @@ import 'package:provider/provider.dart';
 import '../viewmodels/weather_viewmodel.dart';
 import 'package:weather_app/views/weather_header.dart';
 import 'hour_weather_tile.dart';
-import 'package:weather_app/utils/weather_gradients.dart';
+import 'package:weather_app/views/weather_background.dart';
+// import 'package:weather_app/utils/weather_gradients.dart';
+
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -18,50 +20,52 @@ class WeatherScreen extends StatelessWidget {
           ? Center(child: CircularProgressIndicator())
           : vm.weather == null
           ? Center(child: Text("Unable to load weather"))
-          : Container(
-              decoration: BoxDecoration(
-                gradient: WeatherGradients.getGradient(
-                  vm.weather!.weatherCondition,                  
-                  isNight: false,
-                ),
-              ),
-              child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      WeatherHeader(),
+          : Stack(
+              children: [
+                WeatherBackground(condition: vm.weather!.weatherCondition, isNight: vm.isNightTime()),
 
-                      SizedBox(height: 30),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        WeatherHeader(),
 
-                      Text(
-                        "12-hour Forecast:",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 18,
-                          color: Colors.black87,
+                        SizedBox(height: 30),
+
+                        Text(
+                          "12-hour Forecast:",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.black87,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      SizedBox(
-                        height: 140,
-                        child: ListView.separated(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: vm.weather!.hourly.length,
-                          itemBuilder: (context, index) {
-                            final hour = vm.weather!.hourly[index];
-                            return HourWeatherTile(forecast: hour);
-                          },
-                          separatorBuilder: (context, index) =>
-                              SizedBox(width: 12),
+                        SizedBox(height: 10),
+                        SizedBox(
+                          height: 140,
+                          child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: vm.weather!.hourly.length,
+                            itemBuilder: (context, index) {
+                              final hour = vm.weather!.hourly[index];
+                              return HourWeatherTile(forecast: hour);
+                            },
+                            separatorBuilder: (context, index) =>
+                                SizedBox(width: 12),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
     );
   }
 }
+
+
+
+//
